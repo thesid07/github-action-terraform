79586d150d11d7289bcfeb38974a8255265a00c1
thesid07 <siddharthjena2002@gmail.com>
2025-06-06 08:59:05 +0530

    Initial Commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-06031e2c49c278c8f"
   instance_type = "t2.micro"
-   count=1
+   count=2
   tags = {
     Name = "HelloWorld"
     test = "dev"
