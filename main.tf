8ce16ef9d686b99decd3b9e168f8b08321aaae0c
ashisnishanka <43777120+ashisnishanka@users.noreply.github.com>
2025-06-06 08:36:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-06031e2c49c278c8f"
   instance_type = "t2.micro"
-   count=2
+   count=1
   tags = {
     Name = "HelloWorld"
     test = "dev"
